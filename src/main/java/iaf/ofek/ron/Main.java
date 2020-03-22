a29a3933b446e846c1316845494d5af8dffb7d16
eli-ah <62499305+eli-ah@users.noreply.github.com>
2020-03-22 13:53:33 +0200

    fixed untracked files

---
+++ src/main/java/iaf/ofek/ron/Main.java
@@ -0,0 +1,12 @@
+package iaf.ofek.ron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Starting Mitug Simulator ===");
+        SpringApplication.run(Main.class);
+    }
+}
